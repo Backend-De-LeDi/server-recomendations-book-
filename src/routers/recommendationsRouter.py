23c7cc2e8a0494bb480692baaa8b030794906bce
ElBrunoX.js <brunolok79@gmail.com>
2025-07-29 20:48:51 -0300

    se actualizo la ruta de recomendaciones que obtiene la lista de libros que le gusta al usuario

--- src/routers/recommendationsRouter.py
+++ src/routers/recommendationsRouter.py
@@ -1,15 +1,33 @@
 from fastapi.responses import JSONResponse
-from fastapi import APIRouter
+from fastapi import APIRouter,Body
 from src.db.queries.bookQuery import getAllBook
 from fastapi.encoders import jsonable_encoder
+from src.validations.bookValidation import BookValidation
+from typing import List
+from rich import print
+from src.utils.generateText import generateText
+from src.client.chromaClient import initClient
+from src.db.queries.bookQuery import countBooks
+
+
+collection = initClient()
 
 recommendationsRouter = APIRouter()
 
-@recommendationsRouter.get('/recommendations')
-async def recommendations_book():
+@recommendationsRouter.post('/recommendations')
+async def recommendations_book(books:List[BookValidation] = Body(...)):
     try:
-        books = await getAllBook()
-        return JSONResponse(content=jsonable_encoder(books))
+
+        baseRecommended = list(map(generateText ,books ))
+
+        numberOfBooks = await countBooks()
+
+        result = collection.query(
+            query_texts=baseRecommended,
+            n_results=numberOfBooks
+        )
+
+        return JSONResponse(content={'ids':result['ids'][0]},status_code=200)
     except Exception as erro:
         print(erro)
         return JSONResponse(
