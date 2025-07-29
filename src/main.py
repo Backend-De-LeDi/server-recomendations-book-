b33b4e57804bbbedbefd55ce6072bf25d3071afb
ElBrunoX.js <brunolok79@gmail.com>
2025-07-29 20:43:44 -0300

    se actualizo las comunicación del main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.routers.bookRouter import bookRouter
 from src.routers.recommendationsRouter import recommendationsRouter
-
+from src.routers.bookSearchRoute import bookSearchRouter
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3402",  # por ejemplo, si usás React
+    "https://tu-dominio.com"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            # orígenes permitidos
+    allow_credentials=True,           # permite cookies/autenticación
+    allow_methods=["*"],              # permite todos los métodos HTTP
+    allow_headers=["*"]               # permite todos los headers
+)
+
 
 app.include_router(router=bookRouter)
 app.include_router(router=recommendationsRouter)
+app.include_router(router=bookSearchRouter)
