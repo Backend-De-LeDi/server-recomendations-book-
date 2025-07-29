0cfed1946fcf6e39ec0f9cb3d0fdc336c707cda2
ElBrunoX.js <brunolok79@gmail.com>
2025-07-29 20:45:08 -0300

    se creo el bloque de código que lee y valida las variables de entorno

--- src/db/connection.py
+++ src/db/connection.py
@@ -1,6 +1,14 @@
 import motor.motor_asyncio
-import os 
+import os
+from dotenv import load_dotenv
 
-client = motor.motor_asyncio.AsyncIOMotorClient(os.environ.get('URL_DB'))
-db = client["ledi-backend"]
+load_dotenv() 
 
+DB_URI = os.getenv("URL_DB")
+if not DB_URI:
+    raise ValueError("No se encontró URL_DB en el entorno.")
+
+client = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
+
+db_name = "ledi-backend"
+db = client[db_name]
