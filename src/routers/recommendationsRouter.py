e20017e4d97497909e3efa20cd0fa5067bc93e84
ElBrunoX.js <brunolok79@gmail.com>
2025-08-08 14:56:18 -0300

    actualizar la función recommendations_book para aceptar una lista de cadenas en lugar de objetos BookValidation

--- src/routers/recommendationsRouter.py
+++ src/routers/recommendationsRouter.py
@@ -15,19 +15,17 @@ collection = initClient()
 recommendationsRouter = APIRouter()
 
 @recommendationsRouter.post('/recommendations')
-async def recommendations_book(books:List[BookValidation] = Body(...)):
+async def recommendations_book(content:List[str] = Body(...)):
     try:
 
-        baseRecommended = list(map(generateText ,books ))
-
-        numberOfBooks = await countBooks()
-
-        result = collection.query(
-            query_texts=baseRecommended,
+       numberOfBooks = await countBooks()
+       
+       result = collection.query(
+            query_texts=content,
             n_results=numberOfBooks
         )
-
-        return JSONResponse(content={'ids':result['ids'][0]},status_code=200)
+       
+       return JSONResponse(content={'ids':result['ids'][0]},status_code=200)
     except Exception as erro:
         print(erro)
         return JSONResponse(
